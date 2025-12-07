0bea551180ef474f2744bd87af0ddef3987b8341
coderpbt <yeasin800398@gmail.com>
2025-12-07 13:04:05 +0600

    version update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  // experimental: {
-  //   turbopackFileSystemCacheForDev: true,
-  // },
+
 };
 
 export default nextConfig;
